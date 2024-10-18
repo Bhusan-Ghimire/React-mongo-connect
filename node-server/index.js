a8ea4ccec4cbf4a95c3c75b6e969e6c8b0736f6a
Bhusan-Ghimire <ssssgggg550@gmail.com>
2024-10-18 22:35:23 +0545

    wildcard route added again

--- node-server/index.js
+++ node-server/index.js
@@ -66,7 +66,10 @@ server.delete('/user/:_id', async (req,res) => {
 })
 
 
-
+// Wildcard route should be defined last of other routes otherwise it'll overtake them
+server.use('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 
 
